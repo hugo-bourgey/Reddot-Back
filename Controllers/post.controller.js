cf5c41a455ec275678064b84b70610abd9a34840
hugo-bourgey <bourgey18@gmail.com>
2023-05-03 15:10:55 +0200

    ajout getPostBySubId

--- Controllers/post.controller.js
+++ Controllers/post.controller.js
@@ -20,6 +20,18 @@ function getPostById(req, res) {
         .catch((err) => res.status(400).json(err));
 }
 
+function getPostsBySubId(req, res) {
+    post.find({postSub: req.params.postSub})
+        .then((result) => {
+            if (result) {
+                res.send(result);
+            } else {
+                res.status(404).send('No posts for this sub');
+            }
+        })
+        .catch((err) => res.status(400).json(err));
+}
+
 function postPost(req, res) {
     if (!req.body.title) {
         return res.status(400).send('Title is required');
@@ -63,4 +75,4 @@ function deletePost(req, res) {
         .catch((err) => {res.status(500).json(err)});
 }
 
-module.exports = { getAllPosts, getPostById, postPost, putPost, deletePost };
+module.exports = { getAllPosts, getPostById, postPost, putPost, deletePost, getPostsBySubId };
--- Routes/post.router.js
+++ Routes/post.router.js
@@ -4,6 +4,7 @@ const postRouter = express.Router();
 
 postRouter.get('/', postController.getAllPosts);
 postRouter.get('/:id', postController.getPostById);
+postRouter.get('/sub/:postSub', postController.getPostsBySubId);
 postRouter.post('/', postController.postPost);
 postRouter.put('/:id', postController.putPost);
 postRouter.delete('/:id', postController.deletePost);
