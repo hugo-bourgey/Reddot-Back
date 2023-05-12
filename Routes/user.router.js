adce3e27e120584dd076a1923e006f2362d619ce
Dylan Jacquet <d.jacquet@apparence.io>
2023-05-12 11:48:08 +0200

    add upvote and downvote functions on back

--- Routes/user.router.js
+++ Routes/user.router.js
@@ -11,7 +11,17 @@ userRouter.put('/subscribe/:id', userController.subscribeToSubreddot);
 userRouter.put('/unsubscribe/:id', userController.unsubscribeToSubreddot);
 userRouter.get('/isSubscribed/:id', userController.isSubscribed);
 userRouter.get('/upvotes/:id', userController.getUserUpvotes);
+userRouter.get('/downvotes/:id', userController.getUserDownvotes);
+userRouter.get('/comments/upvotes/:id', userController.getUserCommentUpvotes);
+userRouter.get('/comments/downvotes/:id', userController.getUserCommentDownvotes);
 userRouter.post('/upvotes/:id', userController.addUserUpvote);
+userRouter.post('/downvotes/:id', userController.addUserDownvote);
+userRouter.post('/comments/upvotes/:id', userController.addUserCommentUpvote);
+userRouter.post('/comments/downvotes/:id', userController.addUserCommentDownvote);
+userRouter.post('/upvotes/remove/:id', userController.removeUserUpvote);
+userRouter.post('/downvotes/remove/:id', userController.removeUserDownvote);
+userRouter.post('/comments/upvotes/remove/:id', userController.removeUserCommentUpvote);
+userRouter.post('/comments/downvotes/remove/:id', userController.removeUserCommentDownvote);
 userRouter.post('/checkpseudo', userController.checkPseudo);
 userRouter.post('/checkmail', userController.checkMail);
 
