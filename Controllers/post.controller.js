97113734b7b6f6b4a1c614b388dd74c417baf7b3
hugo-bourgey <bourgey18@gmail.com>
2023-05-15 16:47:33 +0200

    tri

--- Controllers/post.controller.js
+++ Controllers/post.controller.js
@@ -10,6 +10,45 @@ function getAllPosts(req, res) {
     .catch((err) => res.status(400).json(err));
 }
 
+function getAllPostsByDate(req, res) {
+    post.find().sort({createdAt:1})
+    .then((result) => {
+        res.send(result);
+    })
+    .catch((err) => res.status(400).json(err));
+}
+
+function getAllPostsByPopularity(req, res) {
+    //post.find().sort({postUpvotes:-1, postDownvotes:1})
+    post.find()
+    .then((result) => {
+        //sort result by number of upvotes
+        result.sort((a,b) => {
+            if (a.postUpvotes.length > b.postUpvotes.length) {
+                return -1;
+            } else if (a.postUpvotes.length < b.postUpvotes.length) {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
+        //sort result descending by number of upvotes and descending by number of downvotes
+        result.sort((a,b) => {
+            if (a.postUpvotes.length === b.postUpvotes.length) {
+                if (a.postDownvotes.length > b.postDownvotes.length) {
+                    return 1;
+                } else if (a.postDownvotes.length < b.postDownvotes.length) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            }
+        });
+        res.send(result);
+    })
+    .catch((err) => res.status(400).json(err));
+}
+
 function getPostById(req, res) {
     post.findById(req.params.id)
     .then((result) => {
@@ -123,4 +162,4 @@ function savePost(req,res, post) {
         .catch((err) => {res.status(500).json(err)});
     }
     
-    module.exports = { getAllPosts, getPostById, postPost, putPost, deletePost, getPostsBySubId };
+    module.exports = { getAllPosts, getPostById, postPost, putPost, deletePost, getPostsBySubId, getAllPostsByDate, getAllPostsByPopularity };
--- Routes/post.router.js
+++ Routes/post.router.js
@@ -3,6 +3,8 @@ const postController = require('../Controllers/post.controller');
 const postRouter = express.Router();
 
 postRouter.get('/', postController.getAllPosts);
+postRouter.get('/date', postController.getAllPostsByDate);
+postRouter.get('/popularity', postController.getAllPostsByPopularity);
 postRouter.get('/:id', postController.getPostById);
 postRouter.get('/sub/:postSub', postController.getPostsBySubId);
 postRouter.post('/', postController.postPost);
